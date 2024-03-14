af1169c06770f3f4698e7d059850218b24fbee87
D.E <cameronqoo@gmail.com>
2024-03-15 01:20:46 +0800

    complete weekly notification scheduler

--- job/report.py
+++ job/report.py
@@ -4,6 +4,7 @@ from model.UserModels import UserData, CreatorType
 from model.WorkModels import Work
 from model.OrderModels import Order
 from model.PorjectModels import Project
+from model.SubscriptionModels import UserSubscription, UserSubscriptionPurview
 from util.teams import send_message
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
@@ -18,14 +19,14 @@ def week_report(force_datetime:datetime|None = None):
     this_year = target_datetime.strftime('%Y')
     this_month = target_datetime.strftime('%m')
     past_year = (target_datetime-relativedelta(months=1)).strftime('%Y')
-    past_year = (target_datetime-relativedelta(months=1)).strftime('%m')
+    past_month = (target_datetime-relativedelta(months=1)).strftime('%m')
     job_db = SessionLocal()
     message = ""
 
     # 1.週報
     message += f"{target_datetime.strftime('%Y-%m-%d')} 週報:\n\n"
     ## 營業額
-    stmt = select(func.coalesce(func.sum(Order.orderFee), 0).label('turnover')).filter(Order.orderStatus == 'completed')
+    stmt = select(func.coalesce(func.sum(Order.orderFee), 0).label('turnover')).where(Order.orderStatus == 'completed')
     result = job_db.execute(stmt).scalar_one()
     message += f"- **營業額: {result}**\r"
     ## 會員總數
@@ -33,21 +34,110 @@ def week_report(force_datetime:datetime|None = None):
     result = job_db.execute(stmt).scalar_one()
     message += f"- **會員總數: {result}**\r"
     ## 創作者總數
-    stmt = select(func.count(UserData.userID).label('creator_count')).filter(UserData.isWork == True)
+    stmt = select(func.count(UserData.userID).label('creator_count')).where(UserData.isWork == True)
     result = job_db.execute(stmt).scalar_one()
     message += f"- **創作者總數: {result}**\r"
     ## 公開作品總數
-    stmt = select(func.count(Work.workID).label('work_count')).filter(Work.publicSetting.in_(['everyone', 'tiers']), Work.isDelete == False)
+    stmt = select(func.count(Work.workID).label('work_count')).where(Work.publicSetting.in_(['everyone', 'tiers']), Work.isDelete == False)
     result = job_db.execute(stmt).scalar_one()
     message += f"- **作品總數: {result}**\r"
     ## 企劃總數
-    stmt = select(func.count(Project.activityID).label('project_count')).filter(Project.isDelete == False)
+    stmt = select(func.count(Project.activityID).label('project_count')).where(Project.isDelete == False)
     result = job_db.execute(stmt).scalar_one()
     message += f"- **企劃總數: {result}**\r"
 
     # 2.訂閱狀況
+    message += '\n\n訂閱狀況:\n\n'
+    ## 取表:
+    with engine.connect() as conn:
+        ### 當月權力表
+        stmt = select(UserSubscriptionPurview).where(
+            UserSubscriptionPurview.subscription_Year == this_year,
+            UserSubscriptionPurview.subscription_Month == this_month
+        ).order_by(
+            UserSubscriptionPurview.userID,
+            UserSubscriptionPurview.creatorID,
+            UserSubscriptionPurview.price.desc()
+        )
+        df_purview = pd.read_sql(stmt, conn)
+        df_purview_normal = df_purview[df_purview['isAddon'] == False].copy()
+        df_purview_normal.drop_duplicates(['userID', 'creatorID'], inplace=True)
+        df_purview_addon = df_purview[df_purview['isAddon'] == True].copy()
+        ### 前月權力表
+        stmt = select(UserSubscriptionPurview).where(
+            UserSubscriptionPurview.subscription_Year == past_year,
+            UserSubscriptionPurview.subscription_Month == past_month
+        ).order_by(
+            UserSubscriptionPurview.userID,
+            UserSubscriptionPurview.creatorID,
+            UserSubscriptionPurview.price.desc()
+        )
+        df_purview_past = pd.read_sql(stmt, conn)
+        df_purview_past_normal = df_purview_past[df_purview_past['isAddon'] == False].copy()
+        df_purview_past_normal.drop_duplicates(['userID', 'creatorID'], inplace=True)
+        df_purview_past_addon = df_purview_past[df_purview_past['isAddon'] == True].copy()
+        ### 當月訂閱表
+        stmt = select(UserSubscription).where(
+            UserSubscription.subscription_Year == this_year,
+            UserSubscription.subscription_Month == this_month,
+            UserSubscription.IsPay == True
+        ).order_by(
+            UserSubscription.userID,
+            UserSubscription.creatorID,
+            UserSubscription.price.desc()
+        )
+        df_subscription = pd.read_sql(stmt, engine)
+        df_subscription_normal = df_subscription[df_subscription['isAddon'] == False].copy()
+        df_subscription_addon = df_subscription[df_subscription['isAddon'] == True].copy()
+    ## 當月總訂閱金額、人數
+    message += "- **當月總訂閱金額: {}**\r".format(pd.concat([df_purview_normal.price, df_purview_addon.price], axis=0).sum())
+    message += "- **當月總訂閱人數: {}**\r".format(len(pd.concat([df_purview_normal.userID, df_purview_addon.userID], axis=0).unique()))
+    ## 新訂閱的金額、人數
+    ### 標準: 這個月有權力，上個月沒有
+    temp_normal = pd.merge(df_purview_normal, df_purview_past_normal, how='left', on=['userID', 'creatorID'])
+    temp_normal = temp_normal[temp_normal['createTime_y'].isnull()]
+    ### 進階: 這個月有權力，上個月沒有
+    temp_addon = pd.merge(df_purview_addon, df_purview_past_addon, how='left', on=['userID', 'creatorID', 'tierID'])
+    temp_addon = temp_addon[temp_addon['createTime_y'].isnull()]
+
+    message += '- **當月新訂閱金額: {}**\r'.format(temp_normal['price_x'].sum() + temp_addon['price_x'].sum())
+    message += '- **當月新訂閱人數: {}**\r'.format(len(pd.concat([temp_normal.userID, temp_addon.userID], axis=0).unique()))
+    ## 預計取消訂閱的金額、人數
+    ### 標準: 有權力，但訂閱為0元方案
+    df_subscription_normal = df_subscription_normal[df_subscription_normal['price'] == 0]
+    temp_normal = pd.merge(df_purview_normal, df_subscription_normal, how='left', on=['userID', 'creatorID'])
+    temp_normal = temp_normal[temp_normal['IsPay'].notnull()]
+    ### 進階: 有權利，但無訂閱
+    temp_addon = pd.merge(df_purview_addon, df_subscription_addon, how='left', on=['userID', 'creatorID', 'tierID'])
+    temp_addon = temp_addon[temp_addon['IsPay'].isnull()]
+
+    message += '- **當月取消訂閱金額: {}**\r'.format(temp_normal['price_x'].sum() + temp_addon['price_x'].sum())
+    message += '- **當月取消訂閱人數: {}**\r'.format(len(pd.concat([temp_normal.userID, temp_addon.userID], axis=0).unique()))
+    
     # 3.創作者類別分布
+    message += '\n\n創作者的類別分布\n\n'
+
+    stmt = select(
+        CreatorType.typeID,
+        func.count(CreatorType.typeID).label('creatorTypeCount')
+    ).group_by(CreatorType.typeID)
+    results = job_db.execute(stmt).all()
+    for typeID, creatorTypeCount in results:
+        message += f"- **{typeID}**: {creatorTypeCount}\r"
     # 4.作品類別分布
+    message += '\n\n作品的類別分布\n\n'
+
+    stmt = select(
+        Work.typeID,
+        func.count(Work.typeID).label('workTypeCount')
+    ).where(
+        Work.isDelete == False,
+        Work.publicSetting != 'self'
+    ).group_by(Work.typeID)
+    results = job_db.execute(stmt).all()
+    for typeID, creatorTypeCount in results:
+        message += f"- **{typeID}**: {creatorTypeCount}\r"
+
     job_db.close()
     # teams
     message = [
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from datetime import datetime
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     scheduler = AsyncIOScheduler()
+    scheduler.add_job(week_report, CronTrigger(day_of_week='mon', hour=10, minute=0, second=0)) #週報
     scheduler.add_job(account_payable, CronTrigger(day=12, hour=12, minute=0, second=0)) #結算名單
     scheduler.add_job(report_receivable, CronTrigger(day='Last', hour=12, minute=0, second=0), args=['proxima']) #回報proxima名單
     scheduler.add_job(report_receivable, CronTrigger(day=10, hour=12, minute=0, second=0), args=['lucselene']) #回報lucselene名單
@@ -44,7 +45,7 @@ app.include_router(user.router)
 def read_root():
     # report_receivable('proxima', datetime(2024, 2, 11, 16, 10, 0))
     # account_payable()
-    week_report()
+    # week_report()
     return {"Hello": "World"}
 
 # uvicorn main:app --reload
