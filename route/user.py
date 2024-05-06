38cb33ef4421986cc746f43899a4cd6503bf2551
D.E <cameronqoo@gmail.com>
2024-05-06 20:14:44 +0800

    add a route to query subscription status.

--- route/user.py
+++ route/user.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Path, Depends, HTTPException, status
 from fastapi.responses import FileResponse
-from sqlalchemy import select, func
+from typing import Annotated
+from sqlalchemy import select, func, delete
+from sqlalchemy.orm import Session, aliased
 from model.UserModels import UserData, CreatorType
-from config.database import engine
+from model.SubscriptionModels import UserSubscription
+from config.database import engine, get_db
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from dateutil.relativedelta import relativedelta
 import tempfile
 
 router = APIRouter(
@@ -59,3 +63,106 @@ async def get_user_data(
         with tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx') as temp_file:
             data.to_excel(temp_file.name, index=False)
     return FileResponse(path=temp_file.name, filename=f'user_list_{datetime.now().strftime("%Y%m%d%H%M%S")}.xlsx', media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+@router.get("/subscribe/now")
+async def show_user_subscribe(db: Annotated[Session, Depends(get_db)],
+                              user_id: list[str] = Query(default=['none'], description='none for not specify.'),
+                              creator_id: list[str] = Query(default=['none'], description='none for not specify.')):
+    # if user_id is None and creator_id is None:
+    #     raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=' not allow both user and creator to be empty data.')
+    user1 = aliased(UserData)
+    user2 = aliased(UserData)
+    stmt = select(
+        UserSubscription.userID,
+        user1.displayName.label('userName'),
+        user1.email.label('userEmail'),
+        UserSubscription.subscription_Year,
+        UserSubscription.subscription_Month,
+        UserSubscription.tierID,
+        UserSubscription.price,
+        UserSubscription.creatorID,
+        user2.displayName.label('creatorName'),
+        user2.email.label('creatorEmail'),
+    ).join(
+        user1, UserSubscription.userID == user1.userID
+    ).join(
+        user2, UserSubscription.creatorID == user2.userID
+    ).where(
+        UserSubscription.subscription_Year == (datetime.now(timezone.utc)+timedelta(hours=8)).strftime('%Y'),
+        UserSubscription.subscription_Month == (datetime.now(timezone.utc)+timedelta(hours=8)).strftime('%m'),
+        UserSubscription.IsPay == 1
+    )
+    if user_id and user_id != ['none']:
+        stmt = stmt.where(
+            UserSubscription.userID.in_(user_id)
+        )
+    if creator_id and creator_id != ['none']:
+        stmt = stmt.where(
+            UserSubscription.creatorID.in_(creator_id)
+        )
+    with engine.connect() as conn:
+        data = pd.read_sql_query(stmt, conn)
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx') as temp_file:
+            data.to_excel(temp_file.name, index=False)
+    return FileResponse(path=temp_file.name, filename=f'user_subscribe_creator_{datetime.now().strftime("%Y%m%d%H%M%S")}.xlsx', media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+@router.get("/subscribe/next")
+async def show_user_subscribe(db: Annotated[Session, Depends(get_db)],
+                              user_id: list[str] = Query(default=['none'], description='none for not specify.'),
+                              creator_id: list[str] = Query(default=['none'], description='none for not specify.')):
+    # if user_id is None and creator_id is None:
+    #     raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=' not allow both user and creator to be empty data.')
+    user1 = aliased(UserData)
+    user2 = aliased(UserData)
+    stmt = select(
+        UserSubscription.userID,
+        user1.displayName.label('userName'),
+        user1.email.label('userEmail'),
+        UserSubscription.subscription_Year,
+        UserSubscription.subscription_Month,
+        UserSubscription.tierID,
+        UserSubscription.price,
+        UserSubscription.creatorID,
+        user2.displayName.label('creatorName'),
+        user2.email.label('creatorEmail'),
+    ).join(
+        user1, UserSubscription.userID == user1.userID
+    ).join(
+        user2, UserSubscription.creatorID == user2.userID
+    ).where(
+        UserSubscription.subscription_Year == (datetime.now(timezone.utc)+timedelta(hours=8)+relativedelta(months=1)).strftime('%Y'),
+        UserSubscription.subscription_Month == (datetime.now(timezone.utc)+timedelta(hours=8)+relativedelta(months=1)).strftime('%m'),
+    )
+    if user_id and user_id != ['none']:
+        stmt = stmt.where(
+            UserSubscription.userID.in_(user_id)
+        )
+    if creator_id and creator_id != ['none']:
+        stmt = stmt.where(
+            UserSubscription.creatorID.in_(creator_id)
+        )
+    with engine.connect() as conn:
+        data = pd.read_sql_query(stmt, conn)
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx') as temp_file:
+            data.to_excel(temp_file.name, index=False)
+    return FileResponse(path=temp_file.name, filename=f'user_subscribe_creator_{datetime.now().strftime("%Y%m%d%H%M%S")}.xlsx', media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+# @router.delete("/subscribe")
+# async def cancel_user_subscribe(db: Annotated[Session, Depends(get_db)],
+#                                 user_id: str = Query(description='欲退訂的使用者ID'),
+#                                 tier_id: str = Query(description='欲退訂的方案ID')):
+#     stmt = select(
+#         UserSubscription
+#     ).where(
+#         UserSubscription.userID == user_id,
+#         UserSubscription.tierID == tier_id,
+#         UserSubscription.subscription_Year == (datetime.now(timezone.utc)+timedelta(hours=8)+relativedelta(months=1)).strftime('%Y'),
+#         UserSubscription.subscription_Month == (datetime.now(timezone.utc)+timedelta(hours=8)+relativedelta(months=1)).strftime('%m')
+#     )
+#     result = db.execute(stmt)
+#     sub = result.scalar_one_or_none()
+#     if sub:
+#         print(sub)
+#         return {'ok': True}
+#     else:
+#         return {'ok': False}
